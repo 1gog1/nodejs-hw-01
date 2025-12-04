4ac336657886d11b0f8f606856eee8caf32cd350
Yehor Bychuk <110350969+1gog1@users.noreply.github.com>
2025-12-04 23:17:47 +0800

    Hellow world1

--- src/server.js
+++ src/server.js
@@ -3,13 +3,17 @@ dotenv.config();
 
 import express from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import { errors as celebrateErrors } from 'celebrate';
 
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
+
+import authRouter from './routes/authRoutes.js';
 import notesRouter from './routes/notesRoutes.js';
+import { authenticate } from './middleware/authenticate.js';
 
 const app = express();
 
@@ -18,10 +22,13 @@ const startServer = async () => {
     await connectMongoDB();
 
     app.use(logger);
-    app.use(cors());
+    app.use(cors({ origin: true, credentials: true }));
     app.use(express.json());
+    app.use(cookieParser());
 
-    app.use(notesRouter);
+    app.use('/auth', authRouter);
+
+    app.use('/notes', authenticate, notesRouter);
 
     app.use(notFoundHandler);
 
---
+++ src/services/auth.js
@@ -0,0 +1,44 @@
+import { v4 as uuid } from "uuid";
+import { Session } from "../models/session.js";
+import { FIFTEEN_MINUTES, ONE_DAY } from "../constants/time.js";
+
+export const createSession = async (userId) => {
+  const accessToken = uuid();
+  const refreshToken = uuid();
+
+  const accessTokenValidUntil = new Date(Date.now() + FIFTEEN_MINUTES);
+  const refreshTokenValidUntil = new Date(Date.now() + ONE_DAY);
+
+  const session = await Session.create({
+    userId,
+    accessToken,
+    refreshToken,
+    accessTokenValidUntil,
+    refreshTokenValidUntil,
+  });
+
+  return session;
+};
+
+export const setSessionCookies = (res, session) => {
+  const cookieOptions = {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+  };
+
+  res.cookie("accessToken", session.accessToken, {
+    ...cookieOptions,
+    maxAge: FIFTEEN_MINUTES,
+  });
+
+  res.cookie("refreshToken", session.refreshToken, {
+    ...cookieOptions,
+    maxAge: ONE_DAY,
+  });
+
+  res.cookie("sessionId", session._id.toString(), {
+    ...cookieOptions,
+    maxAge: ONE_DAY,
+  });
+};
